9dab0d57e3194bc9bdbf63614982108b23edbf16
vallenti <vallentin.k@gmail.com>
2016-09-29 18:36:17 +0300

    Pushing new exercises

---
+++ Projects/Data-Types-and-Variables-Exercices/07Strings-and-Objects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06Strings_and_Objects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hello = "Hello";
+            string world = "World";
+            Console.WriteLine("{0} {1}", hello, world);
+        }
+    }
+}
---
+++ Projects/Data-Types-and-Variables-Exercices/Exchange-Variable-Values/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07Exchange_Variable_Values
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            int b = 10;
+            int aAfter = 0;
+            int bAfter = 0;
+            Console.WriteLine("Before:");
+            Console.WriteLine("a = {0}", a);
+            Console.WriteLine("b = {0}", b);
+            aAfter = b;
+            bAfter = a;
+            Console.WriteLine("After:");
+            Console.WriteLine("a = {0}", aAfter);
+            Console.WriteLine("b = {0}", bAfter);
+        }
+    }
+}
